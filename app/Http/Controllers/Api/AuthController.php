bc0d6cfed079cc133bfdeba40f7da22a8c4a1bdc
Nuraliza <nuralizashafira29@gmail.com>
2025-11-18 14:36:28 +0700

    nambah fungsion di AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,7 +9,7 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    // Dummy user (1 akun saja)
+    // Dummy user
     private $user = [
         'id' => 1,
         'name' => 'User Satu',
@@ -17,6 +17,29 @@ class AuthController extends Controller
         'password' => 'password123',
         'role' => 'user'
     ];
+
+    // REGISTER
+    public function register(Request $request)
+    {
+        $validated = $request->validate([
+            "name" => "required|string|max:100",
+            "email" => "required|email",
+            "password" => "required|string|min:6|confirmed"
+        ]);
+
+        $newUser = [
+            'id' => rand(3, 999),
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => $validated['password'],
+            'role' => 'user'
+        ];
+
+        return response()->json([
+            "message" => "User registered successfully (dummy)",
+            "user" => $newUser
+        ], 201);
+    }
 
     // LOGIN
     public function login(Request $request)
@@ -50,7 +73,7 @@ class AuthController extends Controller
         ]);
     }
 
-    // PROFILE (Protected)
+    // PROFILE
     public function profile(Request $request)
     {
         $payload = $request->jwt_payload;
@@ -60,5 +83,21 @@ class AuthController extends Controller
             'name' => $payload->get('name'),
             'role' => $payload->get('role')
         ]);
+    }
+
+    // LOGOUT
+    public function logout()
+    {
+        try {
+            JWTAuth::invalidate(JWTAuth::getToken());
+
+            return response()->json([
+                'message' => 'User logged out successfully'
+            ]);
+        } catch (\Exception $e) {
+            return response()->json([
+                'message' => 'Failed to logout'
+            ], 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,15 +9,16 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+// AUTH  (JWT)
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['dummy.jwt'])->group(function () {
     Route::get('/profile', [AuthController::class, 'profile']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
-//Categories
-use App\Http\Controllers\Api\CategoryController;
-
+// CATEGORIES
 Route::post('/categories', [CategoryController::class, 'store']);
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::put('/categories/{id}', [CategoryController::class, 'update']);
